2296e4e111f150ff0e8ed1eb7495679e1b422a12
Isaac Spanier <ispanier@iastate.edu>
2019-09-10 01:44:14 -0500

    Added test for basic looping and figured out how to run the java files in VSCode

--- Experiments/Isaac/helloWorld.java
+++ Experiments/Isaac/helloWorld.java
@@ -2,7 +2,7 @@
  * Hello World example for java within VSCode
  */
 
- public class HelloWorld {
+ public class helloWorld {
      
         public static void main(String[] args){
             System.out.println("Hello World");
--- Experiments/Isaac/test.java
+++ Experiments/Isaac/test.java
@@ -1,7 +1,16 @@
-public class HelloWorld {
+public class test {
 
+    /**
+     * Still can't figure out how to run this in a less ugly way that using the
+     * Java Process Console
+     * 
+     * Need a way to just print and not cd 
+     * @param args
+     */
     public static void main(String[] args) {
-        
-        System.out.println("Hello World");
+        int i;
+        for(i = 0; i < 5; i ++) {
+            System.out.println("This is line: " + i);
+        }
     } 
 }
