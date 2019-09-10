5189f500aedb045b8424a8c941d7a31e13fe7a83
Isaac Spanier <ispanier@iastate.edu>
2019-09-10 01:30:27 -0500

    added a new file to try and test out the java jdk

---
+++ Experiments/Isaac/helloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Hello World example for java within VSCode
+ */
+
+ public class HelloWorld {
+     
+        public static void main(String[] args){
+            System.out.println("Hello World");
+        }
+}
