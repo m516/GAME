1d9e830b5dc3448b4a6b7a414ba0360c587224c7
ashtonn <ashtonn@iastate.edu>
2019-09-10 15:20:30 -0500

    Window display a rectangle

--- experiments/ashton/main.cpp
+++ experiments/ashton/main.cpp
@@ -1,9 +1,13 @@
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
 
 int main()
 {
-	sf::Window window(sf::VideoMode(800, 600), "My Window");
-	
+	sf::RenderWindow window(sf::VideoMode(800, 600), "My Window", sf::Style::Default);
+	sf::RectangleShape rect(sf::Vector2f(120.f, 50.f));
+	rect.setFillColor(sf::Color(100, 250, 50));
+
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -12,6 +16,10 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+
+		window.clear(sf::Color::Black);
+		window.draw(rect);
+		window.display();
 	}
 
 	return 0;
