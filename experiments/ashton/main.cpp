12e1e3009a981fae30d6793a003cda8d6fbcde74
Ash <ashton.nelson98@gmail.com>
2019-09-12 01:07:31 -0500

    Tested window scaling and threading for events

--- experiments/ashton/main.cpp
+++ experiments/ashton/main.cpp
@@ -1,26 +1,82 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <thread>
+#include <X11/Xlib.h>
+
+void EventHandler(sf::RenderWindow &);
+
+bool temp = true;
 
 // Testing stuff
 int main()
 {
+	XInitThreads();
 	// Create a window with rendoring
-	sf::RenderWindow window(sf::VideoMode(800, 800), "My Window", sf::Style::Default);
-	window.setTitle("G.A.M.E.");
+	sf::RenderWindow window(sf::VideoMode(256, 256), "G.A.M.E.", sf::Style::Default);
 	// Create a rectangle named "rect"
-	sf::RectangleShape rect(sf::Vector2f(120.f, 50.f));
-	rect.setFillColor(sf::Color(100, 250, 50));
+	sf::RectangleShape rect(sf::Vector2f(200, 50.f));
 
 	// Load font
 	sf::Font font;
-	font.loadFromFile("BitScript.ttf");
-
-	bool temp = true;
+	// font.loadFromFile("BitScript.ttf");
+	font.loadFromFile("bitwise.ttf");
+	window.setSize(sf::Vector2u(1600, 1600));
+	// bool temp = true;
 	// Keep running while the app runs
+	std::thread thread_obj(EventHandler, std::ref(window));
+	// thread_obj.join();
 	while (window.isOpen())
 	{
 		// Close the window on close
+		
+
+		// Update Window //
+		// Clear window with color black
+		window.clear(sf::Color::Black);
+		
+		// Get window size
+		sf::Vector2u windowSize = window.getSize();
+
+		// Draw welcome text
+		sf::Text text;
+		text.setFont(font);
+		text.setString("Welcome to G.A.M.E.");
+		text.setPosition((windowSize.x / 2) - 200, (windowSize.y / 2) - 50);
+		text.setCharacterSize(32); // In Pixels
+		text.setFillColor(sf::Color::White);
+		window.draw(text);
+
+		if (temp)
+		{
+			// Change text and draw
+			text.setString("Option 1");
+			text.setPosition(200, 200);
+			window.draw(text);
+			// Change rectangle color & draw
+			rect.setFillColor(sf::Color(100, 250, 50));
+			window.draw(rect);
+		}
+		else
+		{
+			text.setString("Option 2");
+			text.setPosition(200, 300);
+			window.draw(text);
+			rect.setFillColor(sf::Color(250, 100, 50));
+			window.draw(rect);
+		}
+
+		// Display the window
+		window.display();
+	}
+
+	return 0;
+}
+
+void EventHandler(sf::RenderWindow &window)
+{
+	while (window.isOpen())
+	{
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
@@ -35,38 +91,5 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-
-		// Update Window //
-		// Clear window with color black
-		window.clear(sf::Color::Black);
-		// Draw rectangle
-		window.draw(rect);
-		sf::Vector2u windowSize = window.getSize();
-		sf::Text text;
-		text.setFont(font);
-		text.setString("Welcome to G.A.M.E.");
-		text.setPosition((windowSize.x / 2) - 200, (windowSize.y / 2) - 50);
-		text.setCharacterSize(32); // In Pixels
-		text.setFillColor(sf::Color::White);
-		window.draw(text);
-
-		if (temp)
-		{
-			text.setString("Option 1");
-			text.setPosition(200, 200);
-			window.draw(text);
-		}
-		else
-		{
-			text.setString("Option 2");
-			text.setPosition(200, 300);
-			window.draw(text);
-		}
-		
-
-		// Display the window
-		window.display();
 	}
-
-	return 0;
 }
