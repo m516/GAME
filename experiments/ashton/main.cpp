766e3991ddf916849176522c0afc782ae86f5123
Ash <ashton.nelson98@gmail.com>
2019-09-18 14:33:02 -0500

    Got resizable screen to work, super messy code :)

--- experiments/ashton/main.cpp
+++ experiments/ashton/main.cpp
@@ -13,6 +13,7 @@ sf::Font font;
 sf::Clock myClock;
 void EventHandler(sf::RenderWindow &);
 void Renderer(sf::RenderWindow &);
+sf::View getLetterboxView(sf::View, int, int);
 
 
 // Testing stuff
@@ -21,14 +22,16 @@ int main()
 	// Need this to do threading with X
 	XInitThreads();
 	// Create a window with rendoring
-	sf::RenderWindow window(sf::VideoMode(256, 256), "G.A.M.E.", sf::Style::Default);
+	sf::RenderWindow window(sf::VideoMode(256, 256), "G.A.M.E.", sf::Style::Resize + sf::Style::Close);
+	
+	
 	window.setFramerateLimit(120);
 	// Load font
 	
 	font.loadFromFile("bitwise.ttf");
 
 	// Scale window up
-	window.setSize(sf::Vector2u(1600, 1600));
+	// window.setSize(sf::Vector2u(1920, 1080));
 	
 	// Keep running while the app runs
 	// Need std::ref to pass by reference
@@ -47,9 +50,50 @@ int main()
 const sf::Time timePerFrame = sf::seconds(1.f/60.f); // 1/60 ~ 60FPS
 sf::Time timeSinceLastUpdate = sf::Time::Zero;
 
+sf::View getLetterboxView(sf::View view, int windowWidth, int windowHeight) {
+
+    // Compares the aspect ratio of the window to the aspect ratio of the view,
+    // and sets the view's viewport accordingly in order to archieve a letterbox effect.
+    // A new view (with a new viewport set) is returned.
+
+    float windowRatio = windowWidth / (float) windowHeight;
+    float viewRatio = view.getSize().x / (float) view.getSize().y;
+    float sizeX = 1;
+    float sizeY = 1;
+    float posX = 0;
+    float posY = 0;
+
+    bool horizontalSpacing = true;
+    if (windowRatio < viewRatio)
+        horizontalSpacing = false;
+
+    // If horizontalSpacing is true, the black bars will appear on the left and right side.
+    // Otherwise, the black bars will appear on the top and bottom.
+
+    if (horizontalSpacing) {
+        sizeX = viewRatio / windowRatio;
+        posX = (1 - sizeX) / 2.f;
+    }
+
+    else {
+        sizeY = windowRatio / viewRatio;
+        posY = (1 - sizeY) / 2.f;
+    }
+
+    view.setViewport( sf::FloatRect(posX, posY, sizeX, sizeY) );
+
+    return view;
+}
+
+sf::View view;
+
 void Renderer(sf::RenderWindow &window)
 {
 	sf::Clock framerate;
+
+	view.setSize(256, 256);
+	view.setCenter(128, 128);
+	view = getLetterboxView(view, 256, 256);
 
 	while (window.isOpen())
 	{
@@ -62,6 +106,7 @@ void Renderer(sf::RenderWindow &window)
 		{
 			delta = timeSinceLastUpdate.asSeconds();
 			window.clear(sf::Color::Black);
+			window.setView(view);
 
 			sf::Vector2u windowSize = window.getSize();
 
@@ -99,6 +144,7 @@ void Renderer(sf::RenderWindow &window)
 				window.draw(rect);
 			}
 
+
 			// Display the window
 			window.display();
 		}
@@ -111,6 +157,12 @@ void EventHandler(sf::RenderWindow &window)
 	sf::Event event;
 	while (window.pollEvent(event)) /// While events are "queued"
 	{
+		if (event.type == sf::Event::Resized)
+		{
+			std::cout << "Resize";
+			view = getLetterboxView(view, event.size.width, event.size.height);
+		}
+
 		// Key Down
 		if (event.type == sf::Event::KeyPressed)
 		{
@@ -135,6 +187,8 @@ void EventHandler(sf::RenderWindow &window)
 					y += speed;
 					break;
 			}
+
+			
 		}
 
 		// Terminate program
