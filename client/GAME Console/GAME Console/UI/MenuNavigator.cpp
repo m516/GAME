c0552a7e4d8ee039520efa1784b4ba557c2b4192
m516 <micah112358@gmail.com>
2019-09-27 12:53:44 -0500

    Begin populating MenuNavigator
    TODO finish implementation code for for up, down, left, right, and exit events

--- client/GAME Console/GAME Console/UI/MenuNavigator.cpp
+++ client/GAME Console/GAME Console/UI/MenuNavigator.cpp
@@ -1,3 +1,36 @@
 #include "MenuNavigator.h"
 
-//TODO implement
+//Initializes the navigator
+MenuNavigator::MenuNavigator() {
+
+}
+//Update the keys being pressed.
+void MenuNavigator::poll() {
+	clear();
+	if (sf::Keyboard::isKeyPressed(_keyEnter)) {
+		enter = 1;
+		current_control = Control::NONE;
+	}
+	//TODO finish for up, down, left, right, and enter
+}
+
+void MenuNavigator::clear() {
+	up = 0;
+	down = 0;
+	left = 0;
+	right = 0;
+	enter = 0;
+	exit = 0;
+	current_control = Control::NONE;
+}
+
+//Bind a key to a character on the keyboard.
+//Default WASD controls
+void MenuNavigator::setKey(Control keyToSet, sf::Keyboard::Key value) {
+	switch (keyToSet) {
+	case Control::ENTER:
+		_keyEnter = value;
+		break;
+		//TODO add cases for up, down, left, right, and enter
+	}
+}
--- client/GAME Console/GAME Console/UI/MenuNavigator.h
+++ client/GAME Console/GAME Console/UI/MenuNavigator.h
@@ -5,33 +5,47 @@
 #include <SFML/System.hpp>
 class MenuNavigator {
 public:
+	//Initializes the navigator
 	MenuNavigator();
+	//Update the keys being pressed.
 	void poll();
-	void setKey();
+	//Bind a key to a character on the keyboard.
+	//Default WASD controls
+	void setKey(control_t controlToSet, char key);
 
+
+	//Controls
 	int up = 0;
 	int down = 0;
 	int left = 0;
 	int right = 0;
 	int enter = 0;
+	int exit = 0;
 
-	enum KEY
+	//MenuNavigator updates the controls only if 
+	//keyboard_enabled is true
+	int keyboard_enabled = 1;
+
+
+	//The list of controls that go into a menu
+	typedef enum CONTROL
 	{
 		UP,
 		DOWN,
 		LEFT,
 		RIGHT,
-		ENTER
-	};
+		ENTER,
+		EXIT
+	} control_t;
 
 private:
+	//Key bindings for each control
 	char _keyUp    = 'w';
 	char _keyDown  = 's';
 	char _keyLeft  = 'a';
 	char _keyRight = 'd';
 	char _keyEnter = '\n';
-
-	
+	char _keyExit  = '\t';
 };
 
 #endif
