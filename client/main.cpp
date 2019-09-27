77cb527914e01a0e893e57200a6c0a7ebe9e0908
Ash <ashton.nelson98@gmail.com>
2019-09-26 20:39:09 -0500

    Added hello world program

---
+++ client/main.cpp
@@ -0,0 +1,13 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <X11/Xlib.h>
+
+int main()
+{
+    std::cout << "This is the main client" << std::endl;
+    return 0;
+}
