fb99b3ad7dafbcb7a7e52ad17fb23a7d4a95de6f
Isaac Spanier <ispanier@iastate.edu>
2019-10-15 11:14:33 -0500

    adding the correct tags to the right files

--- GameProject.zip_expanded/GameProject/src/main/java/sr5/project/GameProject/Users.java
+++ GameProject.zip_expanded/GameProject/src/main/java/sr5/project/GameProject/Users.java
@@ -1,5 +1,21 @@
 package sr5.project.GameProject;
 
-public class Users {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "users")
+public class Users {
+	private int userId;
+	private String userName;
+	private String email;
+	private String password; 
+	
+	private int userLevel;
+	
+	public User()
+	
 }
--- GameProject.zip_expanded/GameProject/src/main/java/sr5/project/GameProject/UsersController.java
+++ GameProject.zip_expanded/GameProject/src/main/java/sr5/project/GameProject/UsersController.java
@@ -1,19 +1,5 @@
 package sr5.project.GameProject;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "users")
 public class UsersController {
-	private int userId;
-	private String userName;
-	private String email;
-	private String password; 
-	
-	private int userLevel;
 	
 }
