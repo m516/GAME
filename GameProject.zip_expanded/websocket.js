9b09f0f23751c6cc58c6a20e15fc4f572c2d364a
Isaac Spanier <ispanier@iastate.edu>
2019-10-18 12:12:58 -0500

    updtaed the ip and host

--- GameProject.zip_expanded/websocket.js
+++ GameProject.zip_expanded/websocket.js
@@ -6,7 +6,7 @@ function connect() {
     var host = document.location.host;
     var pathname = document.location.pathname;
     
-    ws = new WebSocket("ws://" +"localhost:8080"+"/websocket" + "/"+username);
+    ws = new WebSocket("ws://" +"coms-309-sr-5.misc.iastate.edu:8080"+"/websocket" + "/"+username);
 
     ws.onmessage = function(event) {
     var log = document.getElementById("log");
