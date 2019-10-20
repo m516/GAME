ed44a145d9d54e66d6b655c5b578a24f1d6e01da
Ash <ashton.nelson98@gmail.com>
2019-10-19 23:48:43 -0500

    Renamed menu folder

---
+++ client/GAMEConsole/src/GUI/menu/menuItem.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <string>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
+#include "../theme.h"
+#include "../guiElement.h"
+
+class MenuItem : public GUIElement{
+public:
+	/**
+	Initializes the MenuItem with the things it needs to have.
+	@param text the text to display on the menu
+	@param function the function to call when the menu item is activated
+	*/
+	MenuItem(Theme *theme, const std::string& text, int (*function_when_pressed) ());
+	/**
+	Deconstructor for MenuItem
+	*/
+	~MenuItem();
+	/**
+	true if pressed, false if otherwise
+	*/
+	bool pressed = false;
+	/**
+	function to call when this menu item is pressed
+	*/
+	int (*pressed_function) ();
+	/**
+	Sets the text to display
+	*/
+	void setText(const std::string& description_text);
+
+
+	//Overridden from GUIElement
+	/**
+	Set the size of the menuItem rendered on the screen
+	*/
+	void setSize(float x, float y);
+	void setSize(sf::Vector2f &new_size);
+
+	/**
+	Set the size of the menuItem rendered on the screen
+	*/
+	void setPosition(float x, float y);
+	void setPosition(sf::Vector2f &new_position);
+
+	/**
+	renders the item as a box with text in it
+	@param target the window onto which this MenuItem renders
+	*/
+	void render();
+private:
+	//string of text to display
+	sf::Text label;
+	//Instance of rectangle border
+	sf::RectangleShape border;
+};
---
+++ client/GAMEConsole/src/GUI/menu/menuPane.h
@@ -0,0 +1,81 @@
+#pragma once
+
+
+#include <vector>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
+#include "menuItem.h"
+#include "../../UI/menuNavigator.h"
+#include "../guiElement.h"
+
+//Shortcut for writing vectors of MenuItems
+typedef std::vector<MenuItem> MenuItem_vec_t;
+
+
+class MenuPane : public GUIElement {
+public:
+	/**
+	Constructs a new MenuPane
+	*/
+	MenuPane(int initial_size);
+
+	/**
+	Adds an item to the list of items to display
+	*/
+	void addItem(MenuItem item);
+
+
+	/**
+	The selected item's index
+	*/
+	unsigned int selected = 0;
+
+
+	/**
+	The MenuNavigator object controlling the menu
+	*/
+	MenuNavigator menuNavigator;
+
+
+	/**
+	Updates the menu
+	*/
+	void update();
+
+
+	/**
+	renders the item onto a RenderWindow
+	@param target the window onto which this MenuItem renders
+	*/
+	void render();
+
+	/**
+	Updates how the GUI element looks
+	Called after each time the position or size is reset
+	*/
+	void updateLook();
+
+	//Overridden from GUIElement
+	/**
+	Set the size of this object rendered on the screen
+	*/
+	void setSize(float x, float y);
+	void setSize(sf::Vector2f& new_size);
+
+	/**
+	Set the position of this object rendered on the screen
+	*/
+	void setPosition(float x, float y);
+	void setPosition(sf::Vector2f& new_position);
+	
+	
+private:
+	/**
+	A collection of Items
+	Contained as std::vector<MenuItem>
+	*/
+	MenuItem_vec_t items;
+};
