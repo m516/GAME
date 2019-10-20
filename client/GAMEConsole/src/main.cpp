505ee3ebb7ab9b49dd9fe62e5804d7d6496cce97
m516 <micah112358@gmail.com>
2019-10-19 23:58:35 -0500

    Remove X11 Libraries
    Aston, please don't add these to the application, as they throw errors while compiling on the Pi and on Windows!

--- client/GAMEConsole/src/main.cpp
+++ client/GAMEConsole/src/main.cpp
@@ -1,10 +1,8 @@
 //Source: https://www.sfml-dev.org/tutorials/2.5/start-linux.php
 #include "application.h"
-#include <X11/Xlib.h>
 
 int main()
 {
-	XInitThreads();
 	Application app;
 	app.run();
 }
