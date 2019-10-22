cc23371a4afc82882e96d920de00c716b2fc26a0
Ash <ashton.nelson98@gmail.com>
2019-10-22 15:44:58 -0500

    Increase minimum frame time

--- client/GAMEConsole/src/modules/renderer.h
+++ client/GAMEConsole/src/modules/renderer.h
@@ -19,7 +19,7 @@ class Renderer
 		sf::View getLetterboxView(sf::View view, int windowWidth, int windowHeight);
 	private:
 		/** Minimum frame time (essentially highest fps possible)*/
-		const sf::Time FRAME_TIME = sf::milliseconds(1.f);
+		const sf::Time FRAME_TIME = sf::milliseconds(10.f);
 		/** Time since the last time the frame updated */
 		sf::Time timeSinceLastFrame = sf::Time::Zero;
 		/** Frame clock to use for tracking frame time */
