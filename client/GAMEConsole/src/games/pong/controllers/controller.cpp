96ee04384743ce3641cd12c6239a7f7e3606bfa3
Micah M <micah112358@gmail.com>
2019-11-05 13:28:39 -0600

    Minor changes
    Former-commit-id: 039363e795a56b8730c6190a2c4a858c3e5bee77

--- client/GAMEConsole/src/games/pong/controllers/controller.cpp
+++ client/GAMEConsole/src/games/pong/controllers/controller.cpp
@@ -3,3 +3,14 @@
 Controller::~Controller(){
 
 }
+
+void Controller::enable() {
+	enabled = true;
+}
+void Controller::disable() {
+	enabled = false;
+}
+
+bool Controller::is_enabled() {
+	return enabled;
+}
