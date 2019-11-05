b6ef70630d8c60178c36f2f337641ecad34c25c0
Micah M <micah112358@gmail.com>
2019-11-05 13:27:11 -0600

    Update controller.h
    Make it abstract
    
    
    Former-commit-id: 54170c33d066d01e7f7de04c358a11660478c73e

--- client/GAMEConsole/src/games/pong/controllers/controller.h
+++ client/GAMEConsole/src/games/pong/controllers/controller.h
@@ -1,46 +1,24 @@
 #pragma once
 
+#include <vector>
+
+#include "..//..//sprite.h"
+
+typedef std::vector<Sprite> sprite_list;
+
 class Controller {
 public:
 
 	//Virtual destructor
 	virtual ~Controller();
 
-	//The list of controls that go into a menu
-	typedef enum class CONTROL
-	{
-		UP,
-		DOWN,
-		LEFT,
-		RIGHT,
-		ENTER,
-		EXIT,
-		NONE
-	} Control;
-
-	typedef enum class STATUS {
-		UP,
-		PRESSED,
-		DOWN,
-		RELEASED,
-		DISABLED
-	} Status;
-
+	//Sets whether or not the controller does anything on update
+	bool enabled = false;
 
 	//Update the keys being pressed.
 	virtual void update() = 0;
 
-
-	//Controls
-	Status up = Status::UP;
-	Status down = Status::UP;
-	Status left = Status::UP;
-	Status right = Status::UP;
-	Status enter = Status::UP;
-	Status exit = Status::UP;
-	Control current_control = Control::NONE;
-
-	//updates the controls only if enabled is true
-	bool enabled = true;
+	//List of Sprite objects to control
+	sprite_list targets;
 };
 
