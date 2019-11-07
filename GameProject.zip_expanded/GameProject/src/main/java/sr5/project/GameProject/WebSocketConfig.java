828412c8d7c087336ec70a29f06039d083d1f0ac
Micah M <micah112358@gmail.com>
2019-11-06 20:22:49 -0600

    Fix merge conflicts
    Former-commit-id: 9a1607cff1eb8fad2e7483bd39c6beeed42fd77f

--- GameProject.zip_expanded/GameProject/src/main/java/sr5/project/GameProject/WebSocketConfig.java
+++ GameProject.zip_expanded/GameProject/src/main/java/sr5/project/GameProject/WebSocketConfig.java
@@ -1,21 +1,17 @@
-<<<<<<< HEAD
 package sr5.project.GameProject;
-=======
-package sr5.project.GameProject; 
->>>>>>> frontend-network-implementation
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 /**
- * 
+ *
  * @author Vamsi Krishna Calpakkam
  *
  */
-@Configuration 
-public class WebSocketConfig {  
-    @Bean  
-    public ServerEndpointExporter serverEndpointExporter(){  
-        return new ServerEndpointExporter();  
-    }  
-} 
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter(){
+        return new ServerEndpointExporter();
+    }
+}
--- GameProject.zip_expanded/GameProject/src/main/java/sr5/project/GameProject/WebSocketServer.java
+++ GameProject.zip_expanded/GameProject/src/main/java/sr5/project/GameProject/WebSocketServer.java
@@ -1,9 +1,6 @@
 package sr5.project.GameProject;
 
-<<<<<<< HEAD
-=======
 
->>>>>>> frontend-network-implementation
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,44 +17,35 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 /**
-<<<<<<< HEAD
- * This class runs the connection between different players while 
+ * This class runs the connection between different players while
  * completing against each other in real time.
- * 
+ *
  * @author Parker Larsen
  */
 @ServerEndpoint("/")
-=======
- * 
- * @author Vamsi Krishna Calpakkam
- *
- */
-@ServerEndpoint("/websocket/{username}")
->>>>>>> frontend-network-implementation
 @Component
 public class WebSocketServer {
-	
+
 	// Store all socket session and their corresponding username.
     private static Map<Session, String> sessionUsernameMap = new HashMap<>();
     private static Map<String, Session> usernameSessionMap = new HashMap<>();
-    
-<<<<<<< HEAD
+
     String username = "game";
     String player = "";
-    
+
     private final Logger logger = LoggerFactory.getLogger(WebSocketServer.class);
-    
+
     @OnOpen
     /**
      * This class is used for the initial connection between the client and websocket
      * @param session the session for this user
-     * @throws IOException 
+     * @throws IOException
      */
     public void onOpen(Session session) throws IOException
     {
         logger.info("Entered into Open" + sessionUsernameMap.size());
     }
- 
+
     @OnMessage
     /**
      * This class allows the user to join a game, define which player they are, and send
@@ -69,19 +57,19 @@ public class WebSocketServer {
     public void onMessage(Session session, String message) throws IOException
     {
     	//logger.info("Entered into Message: Got Message:"+message);
-    	
+
     	//Start with a "!" to join a game and define player number
     	if(message.startsWith("!"))
     	{
     		username = "" + message.charAt(1); //The game number is stored in the first bit
     		player = "" + message.charAt(2);  //The player number is stored in the second bit
     		//logger.info("Player " + player + " has joined game " + username);
-    		
+
             sessionUsernameMap.put(session, username); //Log the session based off the game number
-            usernameSessionMap.put(username, session); 
-            
+            usernameSessionMap.put(username, session);
+
             logger.info("This is user " + sessionUsernameMap.size());
-            
+
             broadcast("Player " + player + " has joined game " + username);
     	}
     	//Send movement information using "."
@@ -100,132 +88,61 @@ public class WebSocketServer {
     	if(message.startsWith("?"))
     	{
     		sendMessageToPArticularUser(
-    				"\nJoin Game: !12: 1-> game number, 2-> the player number" + 
+    				"\nJoin Game: !12: 1-> game number, 2-> the player number" +
     				"\nPlayer Movement: .XXYY, XX-> X coordinate, YY-> Y coordinate");
     	}
     	else // Message to whole chat
-    	{
-=======
-    private final Logger logger = LoggerFactory.getLogger(WebSocketServer.class);
-    
-    @OnOpen
-    public void onOpen(
-    	      Session session, 
-    	      @PathParam("username") String username) throws IOException 
-    {
-        logger.info("Entered into Open");
-        
-        sessionUsernameMap.put(session, username);
-        usernameSessionMap.put(username, session);
-        
-        String message="User:" + username + " has Joined the Chat";
-        	broadcast(message);
-		
-    }
- 
-    @OnMessage
-    public void onMessage(Session session, String message) throws IOException 
-    {
-        // Handle new messages
-    	logger.info("Entered into Message: Got Message:"+message);
-    	String username = sessionUsernameMap.get(session);
-    	
-    	if (message.startsWith("@")) // Direct message to a user using the format "@username <message>"
-    	{
-    		String destUsername = message.split(" ")[0].substring(1); // don't do this in your code!
-    		sendMessageToPArticularUser(destUsername, "[DM] " + username + ": " + message);
-    		sendMessageToPArticularUser(username, "[DM] " + username + ": " + message);
-    	}
-    	else // Message to whole chat
-    	{
-	    	broadcast(username + ": " + message);
->>>>>>> frontend-network-implementation
     	}
     }
- 
+
     @OnClose
-<<<<<<< HEAD
     /**
      * This class closes the session after the user is done.
      * @param session session for the user
      * @throws IOException
      */
-=======
->>>>>>> frontend-network-implementation
     public void onClose(Session session) throws IOException
     {
     	logger.info("Entered into Close");
-    	
+
     	String username = sessionUsernameMap.get(session);
     	sessionUsernameMap.remove(session);
     	usernameSessionMap.remove(username);
-        
+
     	String message= username + " disconnected";
         broadcast(message);
     }
- 
+
     @OnError
-<<<<<<< HEAD
     /**
      * This class displays an error message in the logger when present
      * @param session session of the user
      * @param throwable
      */
-=======
->>>>>>> frontend-network-implementation
-    public void onError(Session session, Throwable throwable) 
+    public void onError(Session session, Throwable throwable)
     {
         // Do error handling here
     	logger.info("Entered into Error");
     }
-<<<<<<< HEAD
     /**
      * Send message to a specific session or game
      * @param message message to send to the session
      */
-	private void sendMessageToPArticularUser(String message) 
-    {	
+	private void sendMessageToPArticularUser(String message)
+    {
 		//String username = "user";
-		
+
     	try {
     		usernameSessionMap.get(username).getBasicRemote().sendText(message);
     		logger.info("sending dm to " + usernameSessionMap.get(username));
-=======
-    
-	private void sendMessageToPArticularUser(String username, String message) 
-    {	
-    	try {
-    		usernameSessionMap.get(username).getBasicRemote().sendText(message);
->>>>>>> frontend-network-implementation
         } catch (IOException e) {
         	logger.info("Exception: " + e.getMessage().toString());
             e.printStackTrace();
         }
     }
-<<<<<<< HEAD
     /**
      * Send message to all the sessions that are being used by the websocket
      * @param message message to be sent
      * @throws IOException
      */
-=======
-    
->>>>>>> frontend-network-implementation
-    private static void broadcast(String message) 
-    	      throws IOException 
-    {	  
-    	sessionUsernameMap.forEach((session, username) -> {
-    		synchronized (session) {
-	            try {
-	                session.getBasicRemote().sendText(message);
-	            } catch (IOException e) {
-	                e.printStackTrace();
-	            }
-	        }
-	    });
-	}
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> frontend-network-implementation
