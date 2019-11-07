7a8d545faaf8cbe6e42d111731c5506fe9f26094
Micah M <micah112358@gmail.com>
2019-11-06 19:29:47 -0600

    Get ready for merge
    Former-commit-id: f610f710c41bdecbfad436d1f7ae310aaa1dba81

--- GameProject.zip_expanded/GameProject/src/main/java/sr5/project/GameProject/HighScoreRepository.java
+++
@@ -1,9 +0,0 @@
-package sr5.project.GameProject;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HighScoreRepository extends CrudRepository<HighScore, Integer>{
-	
-}
