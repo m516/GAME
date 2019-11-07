ddde915ab7ff20abb65a1b0afbf951bcca92e80d
Micah M <micah112358@gmail.com>
2019-11-06 19:47:08 -0600

    Temporarily remove GameProject.zip_expanded
    Former-commit-id: 2451fe24f5ccd4e190004aabcc80b717ba3d1b56

--- GameProject.zip_expanded/GameProject/src/main/java/sr5/project/GameProject/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package sr5.project.GameProject;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Integer>{
- 
-}
