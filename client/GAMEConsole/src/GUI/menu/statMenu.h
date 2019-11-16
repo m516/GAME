a75389dd366edbd500ab8275e956f7c0a434d56f
Isaac Spanier <ispanier@iastate.edu>
2019-11-16 10:13:22 -0600

    added updates the statMenu files
    Former-commit-id: 7e4c347c7b1eecb718df3a1765c65f26f420dcc5

--- client/GAMEConsole/src/GUI/menu/statMenu.h
+++ client/GAMEConsole/src/GUI/menu/statMenu.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include "../lockingElement.h"
+
+class StatMenu : public LockingElement
+{
+public: 
+	StatMenu(sf::RenderWindow* window, Theme* theme);
+
+	~StatMenu();
+
+	//Inherited from GUIElement
+	void render();
+
+private: 
+
+
+
+
+
+
+};
