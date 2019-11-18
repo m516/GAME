dba7fa1bc049811fd0804f1bd11061041bf473af
Ash <ashton.nelson98@gmail.com>
2019-11-18 15:34:50 -0600

    Added mock profile menu
    Former-commit-id: 83e69f0b8aeb9cc2c1b9bf49f0bb1a49ecd0b566

--- client/GAMEConsole/src/GUI/menu/profileMenu.h
+++ client/GAMEConsole/src/GUI/menu/profileMenu.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "../lockingElement.h"
-
+#include <string>
 
 class ProfileMenu : public LockingElement
 {
@@ -12,11 +12,15 @@ class ProfileMenu : public LockingElement
         void render(); // Inherits from GUIElement
 
     private:
-        sf::Text title = sf::Text();
         sf::Text username = sf::Text();
+        sf::Text usernameLabel = sf::Text();
+        sf::Text userID = sf::Text();
+        sf::Text userIDLabel = sf::Text();
         sf::Text level = sf::Text();
         sf::Text friends = sf::Text();
+        sf::Text friendsLabel = sf::Text();
         sf::Text games = sf::Text();
+        sf::Text gamesLabel = sf::Text();
         sf::Text playTime = sf::Text();
 
         sf::RectangleShape levelSquare = sf::RectangleShape(sf::Vector2f(2, 2));
