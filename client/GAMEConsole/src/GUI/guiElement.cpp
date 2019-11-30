faa607cf7209a588997034e308d6e1e1fc463380
Ash <ashton.nelson98@gmail.com>
2019-11-30 14:42:59 -0600

    Cleanup GUI
    Former-commit-id: d7ab45418f4d5279a3fb70271c1cdc64be74c514

--- client/GAMEConsole/src/GUI/guiElement.cpp
+++ client/GAMEConsole/src/GUI/guiElement.cpp
@@ -1,67 +1,76 @@
 #include "guiElement.h"
 
 //Virtual destructor
-GUIElement::~GUIElement() {
+GUIElement::~GUIElement() 
+{
 
 }
 
 /**
 Sets the RenderTarget to render onto
 */
-void GUIElement::setRenderer(sf::RenderWindow* new_renderer) {
+void GUIElement::setRenderer(sf::RenderWindow* new_renderer) 
+{
 	renderer = new_renderer;
 }
-
 
 /**
 Gets the RenderTarget to render onto
 */
-sf::RenderWindow* GUIElement::getRenderer() {
+sf::RenderWindow* GUIElement::getRenderer() 
+{
 	return renderer;
 }
 
 /**
 Set the size of the GUI Element rendered on the screen
 */
-void GUIElement::setSize(float x, float y) {
+void GUIElement::setSize(float x, float y) 
+{
 	size.x = x;
 	size.y = y;
 }
-void GUIElement::setSize(sf::Vector2f& new_size) {
+
+void GUIElement::setSize(sf::Vector2f& new_size) 
+{
 	setSize(new_size.x, new_size.y);
 }
 
-Theme* GUIElement::getTheme() {
+Theme* GUIElement::getTheme() 
+{
 	return theme;
 }
 
-void GUIElement::setTheme(Theme* theme) {
+void GUIElement::setTheme(Theme* theme) 
+{
 	this->theme = theme;
 }
-
 
 /**
 Get the size of the GUI Element rendered on the screen
 */
-sf::Vector2f GUIElement::getSize() {
+sf::Vector2f GUIElement::getSize() 
+{
 	return size;
 }
 
 /**
 Set the position of the GUI Element rendered on the screen
 */
-void GUIElement::setPosition(float x, float y) {
+void GUIElement::setPosition(float x, float y) 
+{
 	position.x = x;
 	position.y = y;
 }
-void GUIElement::setPosition(sf::Vector2f& new_position) {
+void GUIElement::setPosition(sf::Vector2f& new_position) 
+{
 	setPosition(new_position.x, new_position.y);
 }
-
 
 /**
 Get the position of the GUI Element rendered on the screen
 */
-sf::Vector2f GUIElement::getPosition() {
+sf::Vector2f GUIElement::getPosition() 
+{
 	return position;
 }
