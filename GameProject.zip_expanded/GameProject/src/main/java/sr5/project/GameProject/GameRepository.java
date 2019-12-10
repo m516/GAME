78bc73b4adc60ada05a16e1c312e2e1444b3ebc5
Isaac Spanier <ispanier@iastate.edu>
2019-12-10 16:42:17 -0600

    added the comments and refactoring for the GameRepository, the GameService, HighScore, and HighScoreController
    Former-commit-id: e49bea9f5838c170bbec3cf6e277ea4d08c4f300

--- GameProject.zip_expanded/GameProject/src/main/java/sr5/project/GameProject/GameRepository.java
+++ GameProject.zip_expanded/GameProject/src/main/java/sr5/project/GameProject/GameRepository.java
@@ -2,6 +2,11 @@ package sr5.project.GameProject;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+/**
+ * Game Repository Class
+ * @author comcy
+ *
+ */
 
 @Repository
 public interface GameRepository extends CrudRepository<Game, Integer> {
--- GameProject.zip_expanded/GameProject/src/main/java/sr5/project/GameProject/GameService.java
+++ GameProject.zip_expanded/GameProject/src/main/java/sr5/project/GameProject/GameService.java
@@ -5,28 +5,61 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Game Service Class
+ * @author comcy
+ *
+ */
 @Service
 public class GameService {
 
+	/**
+	 * repo is the instance of the Game Repository
+	 */
     @Autowired
     private GameRepository repo;
 
+    /**
+     * Get All Game
+     * @return find all games from the repo
+     */
     public List<Game> getAllGame(){
         return (List<Game>) repo.findAll();
     }
 
+    /**
+     * Save Game
+     * Saves the instance of the game to the repo
+     * @param game
+     */
     public void saveGame(Game game){
         repo.save(game);
     }
 
+    /**
+     * Get Game
+     * @param id
+     * @return the game with the corresponding id 
+     */
     public Game getGame(int id){
         return repo.findById(id).get();
     }
 
+    /**
+     * Delete Game 
+     * Deletes the Game with the associated Id
+     * @param id
+     */
     public void deleteGame(int id){
         repo.deleteById(id);
     }
 
+    /**
+     * Update Game
+     * Updates the game with the associated id
+     * @param id
+     * @param game
+     */
     public void updateGame(int id, Game game){
         repo.save(game);
     }
