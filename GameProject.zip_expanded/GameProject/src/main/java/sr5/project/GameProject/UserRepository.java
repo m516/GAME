a6214aef0f7a1877ffaab8ed472894a8c1a25e1e
Isaac Spanier <ispanier@iastate.edu>
2019-12-11 19:40:17 -0600

    fixing the merge issue
    Former-commit-id: df084f68a5261ade4e96a1933fd969f4d3860d25

--- GameProject.zip_expanded/GameProject/src/main/java/sr5/project/GameProject/UserRepository.java
+++ GameProject.zip_expanded/GameProject/src/main/java/sr5/project/GameProject/UserRepository.java
@@ -3,6 +3,11 @@ package sr5.project.GameProject;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * User Repository Class
+ * @author comcy
+ *
+ */
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer>{
  
